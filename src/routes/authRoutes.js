a2b4f49abc961a1d86edac0385884235fcd92de0
Matus <evelyn.manuel.22s@utzmg.edu.mx>
2026-02-11 14:59:40 -0600

    must

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, login, getProfile } from '../controllers/authController.js';
+import { register, login, getProfile, googleLogin } from '../controllers/authController.js';
 import { verifyToken } from '../middleware/auth.js';
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
 router.get('/profile', verifyToken, getProfile);
+router.post('/google', googleLogin);
+router.get('/google/callback', googleLogin);
 
 export default router;
